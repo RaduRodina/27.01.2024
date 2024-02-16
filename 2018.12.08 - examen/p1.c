476358438b9d6b08924668421e22b994780269e0
RaduRodina <157900054+RaduRodina@users.noreply.github.com>
2024-02-16 23:19:51 +0200

    Update p1.c

--- 2018.12.08 - examen/p1.c
+++ 2018.12.08 - examen/p1.c
@@ -5,114 +5,114 @@
 int convert(char w[])
 {
     int nr = 0;
-    printf("w = %s\n", w);
-    for(int j = 0; j < strlen(w); j++)
+    for (int i = 0; i < strlen(w); i++)
     {
-        if(isdigit(w[j]))
+        if (isdigit(w[i]))
         {
-            nr = nr*10 + (w[j] - '0');
+            nr = nr*10 + (w[i] - '0');
         }
     }
-    printf("nr = %d\n", nr);
     return nr;
 }
 
+
 void citire()
 {
-    int max = -1;
     char c;
+    int n;
     char w[10];
     int j = 0;
-    int stare = 0;
-    int n;
+    int st = 0;
+    int max = -1;
 
     FILE *f;
-    f = fopen("linie.txt", "rt");
+    f = fopen("input.txt", "rt");
+
     if(f == NULL)
     {
         printf("Eroare la intrare\n");
         return;
     }
 
-    c = fgetc(f);
     while(1)
     {
-        w[j]=0;
-        printf("stare = %d c = %d w = %s\n", stare, c, w);
-
-        if(stare == 0)
+        w[j] = 0;
+        printf("stare = %d c = %d w = %s\n", st, c, w);
+        if(st == 0)
         {
             if(isdigit(c))
             {
                 w[j] = c;
                 j++;
-                stare = 1;
                 c = fgetc(f);
+                st = 1;
             }
             else if (c == 10)
             {
-                stare = 3;
+                st = 3;
             }
             else
             {
                 c = fgetc(f);
-                stare = 0;
+                st = 0;
             }
         }
-        else if (stare == 1)
+        else if(st == 1)
         {
             if(isdigit(c))
             {
                 w[j] = c;
                 j++;
                 c = fgetc(f);
-                stare = 1;
+                st = 1;
             }
+
             else if (c == ' ' || c == 10)
             {
-                stare = 2;
+                st = 2;
             }
+
             else
             {
-                stare = 4;
+                st = 4;
             }
         }
-        else if (stare == 2)
+        else if (st == 2)
         {
             n = convert(w);
             printf("n = %d\n", n);
-            if(n>max)
+
+            if(n > max)
             {
                 max = n;
             }
-
             w[j] = 0;
             printf("%s\n", w);
             j = 0;
-            w[0] = 0;
-            stare = 0;
+            w[j] = 0;
+            st = 0;
+        }
+            else if (st == 3)
+            {
+                break;
+            }
+            else if (st == 4)
+            {
+                w[0] = 0;
+                j = 0;
+                st = 0;
+            }
+        }
 
-
-        }
-        else if (stare == 3)
-        {
-            break;
-        }
-        else if(stare == 4)
-        {
-            printf("Dupa numar nu am primit spatiu sau rand nou\n");
-            w[0] = 0;
-            j = 0;
-            stare = 0;
-        }
-    }
     fclose(f);
 
     printf("max = %d\n", max);
 }
 
+
 int main()
 {
     citire();
+
     return 0;
 }
