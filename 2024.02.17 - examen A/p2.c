453dcc474536cfddd4879278afaa1775d7c88cf2
RaduRodina <157900054+RaduRodina@users.noreply.github.com>
2024-02-19 00:11:57 +0200

    Add files via upload

---
+++ 2024.02.17 - examen A/p2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+void right_shift(uint32_t n)
+{
+    uint32_t a;
+
+    printf("initial n = %x\n", n);
+
+    for(int i = 0; i < 8*sizeof(n)/4; i++)
+    {
+        a = n & 0xF;
+        //printf("a = %x\n", a);
+        n = n >> 4;
+        //printf("%x\n", n);
+        n = n | (a<<(8*sizeof(n)-4));
+        printf("n = %x\n", n);
+    }
+}
+
+int main()
+{
+    uint32_t x = 0x123456AB;
+    right_shift(x);
+    return 0;
+}
