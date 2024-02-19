f922e26d2035836440523a6b8f9f3629cfb56327
RaduRodina <157900054+RaduRodina@users.noreply.github.com>
2024-02-19 20:58:01 +0200

    Add files via upload

---
+++ 2024.02.18 - culegere/p2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+void rot_left(uint32_t n)
+{
+    for(int i = 0; i < 8*sizeof(n)/4; i++)
+    {
+        uint32_t msn = n & (0xF<<(8*sizeof(n) - 4));
+        printf("msn = %x\n", msn);
+        n = n << 4;
+        n = n | (msn >> (8*sizeof(n) - 4));
+        printf("n = %x\n", n);
+    }
+}
+
+
+int main()
+{
+    uint32_t x = 0x12345678;
+    rot_left(x);
+
+    return 0;
+}
